29b2cf431216701d39877a8d9e22624b3f13ea39
JB <berginjm@gmail.com>
2025-12-25 07:15:27 -0500

    Add notification hooks and stub service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.Extensions.Hosting;
+using GameSwap.Functions.Notifications;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
@@ -8,6 +9,7 @@ var host = new HostBuilder()
         var tableServiceClient = GameSwap.Functions.Storage.TableClients.CreateServiceClient(context.Configuration);
         services.AddSingleton(tableServiceClient);
         services.AddHostedService<GameSwap.Functions.Storage.TableStartup>();
+        services.AddSingleton<INotificationService, NoOpNotificationService>();
     })
     .Build();
 
